7e6a3b550ad1a4f57989d327c6b2b3ee16973fbf
Draez <oskar.lehtonen@student.samk.fi>
2023-08-20 20:18:12 +0300

    Change input name

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -105,7 +105,7 @@ const Header: FC = () => {
 
                 <div className="mb-8 max-w-md">
                   <div className="w-full mx-auto md:py-2 py-4 pr-0 flex items-center rounded-md bg-light-blue">
-                    <input type="email" name="email" ref={emailRef} placeholder="Sähköpostiosoitteesi.."
+                    <input required type="text" name="email" ref={emailRef} placeholder="Sähköposti"
                       className="flex-1 appearance-none rounded bg-light-blue text-white focus:outline-none ml-4" />
                     <button type="submit"
                       className="bg-main-red hidden md:block text-white text-base font-semibold rounded-md shadow-md hover:bg-main-red-hover p-4 px-6 mr-4">Osallistu</button>
